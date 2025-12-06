cef47e5a69a3fe3c3602413b5d17cfdc81e06148
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-12-06 21:37:39 +0530

    Variable and Datatype in Python

---
+++ Ch-2-Variables-&-Datatypes/Datatypes.py
@@ -0,0 +1,9 @@
+a = 1 # a is an integer
+
+b = 44.45 # b is the floating point number
+
+c = "Avishkar" # c is a string
+
+d = False # d is a boolean variable
+
+e = None # e is none type variable
---
+++ Ch-2-Variables-&-Datatypes/Variables.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+c = 7
+
+name = "Avishkar"
+
+print(a + b)
