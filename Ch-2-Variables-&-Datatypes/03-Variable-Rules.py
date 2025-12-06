c692efc6a3597ba6b048a78d9ab78b3020a33560
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-12-06 21:44:02 +0530

    Variable rules and Operators

---
+++ Ch-2-Variables-&-Datatypes/03-Variable-Rules.py
@@ -0,0 +1,20 @@
+a = 23
+
+aaaa = 23
+
+avishkar = 26362
+
+rohit = 434
+
+_rohit = 455
+
+# @rohit = 4943
+
+aa363 = 3733
+
+print(363)
+
+# a variable name can contain alphabets,digits,and underscores
+# a variable name can only starts with an alphabet or underscores
+# a variable name cant start with digit
+# no white space is allowed to use inside the variable name
---
+++ Ch-2-Variables-&-Datatypes/04-Operators.py
@@ -0,0 +1,52 @@
+# 7+4=11 :-(7&4)=Operands (+)=Operator (11)=Result
+
+# Arithmetic Opertaors
+print("Arithmetic Operator")
+a = 45
+b = 5
+c = a + b
+print(c)
+
+
+# Assignment Operators
+print("Assignment Operator")
+a = 45-5 # Assign 45-5 in a
+print(a)
+b = 10
+# b += 5  Increment the value of b by 5 and then assign it to b
+b -= 5  # Decrement the value of  b by 5 and assign vit to b
+print(b)
+
+# Comparison Operators
+print("Comparison Operator")
+d = 5>=4
+print(d)
+e = 5<=4
+print(e)
+f = 5==4
+print(f)
+g = 5>4
+print(g)
+h = 5!=4
+print(h)
+
+# Logical Operators
+print("Logical Operator")
+# Truth table of OR
+print("OR Operator")
+print("True or False =", True or False)
+print("True or True =", True or True)
+print("False or True =", False or True)
+print("False or False =", False or False)
+
+# Truth table of AND
+print("AND Operator")
+print("True and False =", True and False)
+print("True and True =", True and True)
+print("False and True =", False and True)
+print("False and False =", False and False)
+
+#Truth table of NOT
+print("NOT Operator")
+print(not(False))
+print(not(True))
