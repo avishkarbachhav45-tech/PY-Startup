a6df91617b8de9c2289737173a8d2350a943589e
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-12-07 22:15:34 +0530

    Type and input in python

---
+++ Ch-2-Variables-&-Datatypes/05-Type.py
@@ -0,0 +1,23 @@
+a = 45.5
+b = str(a) # a but the type should be str
+t  = type(b)
+
+print(t)
+
+a = "45.5"
+b = float(a) # a but the type should be float
+t  = type(b)
+
+print(t)
+
+a = 45
+b = str (a) # a but the type should be str
+t  = type(b)
+
+print(t)
+
+a = "32"
+b = int(a) # a but the type should be str
+t  = type(b)
+
+print(t)
