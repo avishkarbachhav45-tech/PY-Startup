f8e1bcc62c85669742f5869a3f15b6763b8c9413
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-12-12 01:20:02 +0530

    Negative slicing and string function

---
+++ Ch-3-Strings/02-Negative-Slicing.py
@@ -0,0 +1,14 @@
+name = "Avishkar"
+
+print(name[0:3])
+
+print(name[-4:-1])
+print(name[4:7])
+
+print(name[:4])
+print(name[1:])
+print(name[1:8])
+
+a = "ayushibharatvispute"
+
+print(a[1:8:5]) # Slicing with skip value
---
+++ Ch-3-Strings/03-str-function.py
@@ -0,0 +1,18 @@
+# Length 
+name = "avishkar"
+print(len(name))
+
+# Endswith & Startswith
+print(name.endswith("kar"))
+print(name.startswith("Avi"))
+
+# Capitalize
+print(name.capitalize())
+
+# Replace function
+a = "Avishkar is a good good boy"
+print(a.replace("good","bad"))
+
+# Find function
+index = a.find("boy")
+print(index)
