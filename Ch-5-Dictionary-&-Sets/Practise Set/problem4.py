6c8ac8a3b6b316575d5dd76e2a5c365386eaa1a5
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-04-03 23:28:14 +0530

    Practice set for chapter 5

---
+++ Ch-5-Dictionary-&-Sets/Practise Set/problem4.py
@@ -0,0 +1,20 @@
+d = {}
+
+name = input("Enter friend name: ")
+lang = input("Enter lang name: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter lang name: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter lang name: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter lang name: ")
+
+d.update({name: lang})
+
+print(d)
