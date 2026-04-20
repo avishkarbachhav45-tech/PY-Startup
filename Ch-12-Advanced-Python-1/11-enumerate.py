0fb529b6df021f7e34309b44e99d595fa1e3f894
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-04-20 15:44:56 +0530

    Advanced python part 1 done

---
+++ Ch-12-Advanced-Python-1/11-enumerate.py
@@ -0,0 +1,10 @@
+l = [3,4,5,6]
+
+# index = 0
+# for item in l:
+#     print(f"The item number {index} is {item}")
+#     index += 1
+
+
+for index, item in enumerate(l):
+        print(f"The item number {index} is {item}")
---
+++ Ch-12-Advanced-Python-1/12-list-comprehensions.py
@@ -0,0 +1,5 @@
+myL = [1,2,3,4,5]
+
+squaredL = [i*i for i in myL]
+
+print(squaredL)
